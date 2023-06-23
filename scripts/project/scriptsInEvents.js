36855f85b4a446cde3f86814dfff9a0dfbe449cb
Attero Labs <43040586+theatterolabs@users.noreply.github.com>
2023-06-23 21:23:38 +0530

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Loader_Event13_Act7(runtime, localVars)
+	{
+		
+	},
+
+	async Loader_Event14_Act9(runtime, localVars)
+	{
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
